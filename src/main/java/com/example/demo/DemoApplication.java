b9820cc99bb5888eebb51c9a34180b5a1aa87501
LoveTheresa <46090064+LoveTheresa@users.noreply.github.com>
2023-03-22 22:58:37 +0800

    Update DemoApplication.java
    pull test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,7 @@ public class DemoApplication {
         System.out.println("third");
         System.out.println("hot-fix");
         System.out.println("git-push");
+        System.out.println("git-pull");
     }
 
 }
